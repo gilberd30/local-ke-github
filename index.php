0add4bf6a309e556482d75010bd6125576e07290
gilberd30 <gilberd30@gmail.com>
2021-06-11 15:27:32 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam debitis assumenda, cum adipisci accusamus maiores eaque delectus illum, a nihil, eveniet quibusdam placeat explicabo incidunt sit dolor iusto vero accusantium.</p>
 </body>
 </html>
